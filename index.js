62c90d20559fe500205f3499d4ec67d98584d108
unknown <doliveirasantos98@gmail.com>
2021-09-20 23:01:35 -0300

    feat: adding JavaScript

---
+++ index.js
@@ -0,0 +1,21 @@
+const buttonOne = document.getElementById('button-1')
+const buttonTwo = document.getElementById('button-2')
+const buttonThree = document.getElementById('button-3')
+const buttonFour = document.getElementById('button-4')
+
+const button = document.querySelectorAll('.btn')
+
+function toggleButton(toggle){
+    if(toggle == 'OFF'){
+        button.innerText = 'ON'
+        button.classList.remove('btn-off')
+        button.classList.add('btn-on')
+    } else {
+        button.innerText = 'OFF'
+        button.classList.add('btn-off')
+        button.classList.remove('btn-on')
+    }
+}
+
+buttonOne.onclick = () => toggleButton(button.innerText)
+buttonTwo.onclick = () => toggleButton(button.innerText)
